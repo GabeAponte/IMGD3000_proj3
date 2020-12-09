05c315af583dc75d71af094a5cc4945e3f4c9b24
Joseph Henry Stadolnik IV <hstadolnik@gmail.com>
2020-12-09 10:43:53 -0500

    Improved shooter stop position

--- stuck-in-space/cpp_files/Enemy.cpp
+++ stuck-in-space/cpp_files/Enemy.cpp
@@ -169,7 +169,7 @@ int Enemy::eventHandler(const df::Event* p_e)
 		// If shooter, stop movement once close enough to the center of the screen
 		if (type == E_SHOOTER) {
 			Vector pixel_position = DM.spacesToPixels(getPosition());
-			Vector pixel_center = WM.getBoundary().getCorner() + DM.spacesToPixels(Vector(WM.getBoundary().getHorizontal() / 2, WM.getBoundary().getVertical() / 2));
+			Vector pixel_center = WM.getBoundary().getCorner() + DM.spacesToPixels(Vector(WM.getBoundary().getHorizontal() / 2, (WM.getBoundary().getVertical() / 2) - 2));
 			if (canMove && abs(pixel_position.getY()-pixel_center.getY()) < SHOOTER_STOP_YDISTANCE && distance(pixel_position, pixel_center) < SHOOTER_STOP_RADIUS) {
 
 				setVelocity(Vector()); // stop movement
